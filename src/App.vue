96cf5c84e6a5f41718487d994acb9be8cfba0c4d
The Anh Pham <admin@phamtheanh.eu>
2022-08-04 11:55:51 +0200

    Using Stylus as CSS preprocessor

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,10 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div id="my-div">THIS IS MY DIV</div>
 </template>
 
-<style>
+<style lang="stylus">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,5 +18,10 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#my-div {
+  padding: 1rem;
+  color: $c-text;
+  background-color: $c-bg;
 }
 </style>
